a699432a66ce1d535e98e7cae51b5046da8b476b
Micah Shute <micah.shute@gmail.com>
2018-07-25 04:35:59 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,134 @@
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  "Mr. Chocolate",
+  3,
+  "M",
+  "brown",
+  "sweet",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  "Rowdy",
+  1,
+  "M",
+  "black",
+  "rowdy",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  "Tabitha",
+  3,
+  "F",
+  "brown",
+  "independent",
+  1
+);
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  "Sergeant Brown",
+  5,
+  "M",
+  "black",
+  "angry",
+  0
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  "Melissa",
+  4,
+  "F",
+  "black",
+  "mischevious",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  "Grinch",
+  6,
+  "M",
+  "black",
+  "sneaky",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  "Wendy",
+  4,
+  "F",
+  "red",
+  "hungry",
+  1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  NULL,
+  5,
+  "M",
+  "brown",
+  "mean",
+  1
+);
